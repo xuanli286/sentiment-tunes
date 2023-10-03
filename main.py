5185cd9c33f2921eedb596670a4bfbff55b82a74
jessicafiore09 <111348094+jessicafiore09@users.noreply.github.com>
2023-10-03 16:09:57 +0800

    inserted text box on main.py
    text box for collecting user input

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import streamlit as st
 import pandas as pd
  
 st.write("""
-# My first app
-Hello *world!*
+# generate your personalised album!
 """)
 
-st.write("""
-# testing hehe
-    below is our dataset
+sentence_input = st.text_input("how was your day?")
+print(sentence_input)
+
+
+st.write(""" 
+your input:
 """)
+st.write(sentence_input)
+
+spotify_link_input = st.text_input("paste your favourite spotify album's link here!")
+print(spotify_link_input)
+
+
  
-df = pd.read_csv("Datasets/collapsed_spotify_result_2020s.csv")
-df_test = df.head(20)
-st.dataframe(df_test)
+# df = pd.read_csv("Datasets/collapsed_spotify_result_2020s.csv")
+# df_test = df.head(20)
+# st.dataframe(df_test)
+
+
